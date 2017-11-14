41dbf357ed613595b28cc2515ef313a20033aad7
David Chen <david@shapetools.com>
2017-11-13 20:30:41 -0800

    Update System Contract source to support async contract call.

--- contractcode/systemcontractv0.1.sol
+++ contractcode/systemcontractv0.1.sol
@@ -3,6 +3,22 @@ pragma solidity ^0.4.11;
 //Moac System Contract
 //
 
+contract Precompiled9 {
+  function localShardCheckAndEnroll (address , address, uint256, uint) returns (uint);
+}
+contract Precompiled10 {
+  function checkShardValid(address, address, uint256) returns(uint);
+}
+
+
+contract Precompiled11 {
+  function queryContract (uint num, address[]) returns (uint);
+}
+
+contract Precompiled12 {
+  function delegateSend (address, address, uint256) returns (uint);
+}
+
 contract MoacSysContract  {
 
     struct delayedSendTask {
@@ -10,14 +26,9 @@ contract MoacSysContract  {
 		address from; //from address
         address to; // target address
         uint256 value;   // value
-        bytes data; //data associated
+        //bytes data; //data associated
     }
     
-    struct delayedQueryTask {
-        uint block; // block# task executed on
-        address to; // target address
-		bytes data; //data associated
-    } 
 
     struct voteProposal {
         uint expireblk;
@@ -26,7 +37,14 @@ contract MoacSysContract  {
 		bool isValue;
     }
     
+	uint constant QueueSize  = 4;
+	Precompiled9 prec9  ; 
+	Precompiled10 prec10; 
+	Precompiled11 prec11; 
+	Precompiled12 prec12; 
+	
     mapping(uint => delayedSendTask[]) sendQueue;
+    mapping(uint => address[]) queryQueue;
     mapping(uint => uint) sendQueueSize;
 
     //version
@@ -44,15 +62,23 @@ contract MoacSysContract  {
 	event DelayArrayFull(uint _blk);
 	event DelayArrayRegistered(uint _blk, address from);
 	event LogStep(string info);
+	event AddQueryTask(uint[] blks, address[] addrs );
+	event QueryContract(uint blk, address[] memaddr);
     
-    //constructor
-    function MoacSysContract() public {
+    //init
+    function init() public {
 		curVersion = 0;
 		curCodeHash = 0;
 		upgradeHash = 0;
 		upgradeWaitingBlock = 0;
 		chkpt = 5;
-    }
+		
+		prec9  = Precompiled9 (0x0000000000000000000000000000000000000009);
+		prec10 = Precompiled10(0x000000000000000000000000000000000000000a);
+		prec11 = Precompiled11(0x000000000000000000000000000000000000000b);
+		prec12 = Precompiled12(0x000000000000000000000000000000000000000c);
+
+	}
 
     //used for SCS to enroll to processing contract in a sharding setup
     function enroll(address _from, uint256 _value) public returns (bool success) {
@@ -98,7 +124,7 @@ contract MoacSysContract  {
 		if( blkarrysize > 0 )
 		{
 			chkpt += 100;
-			if( blkarrysize >= 4 )
+			if( blkarrysize >= QueueSize )
 			{
 				//array full
 				DelayArrayFull(_blk);
@@ -135,17 +161,7 @@ contract MoacSysContract  {
         task.block = _blk;
         task.to = _to;
         task.value = _value;
-        
-    	uint j=0;
-    	task.data = new bytes(32);
-
-		//address
-		for(  j=0; j< 12; j++ )
-			task.data[j] = 0;
-
-		for(  j=0; j<20; j++ )
-			task.data[12+j] = byte(uint8(uint(msg.sender) / (2**(8*(19 - j)))));
-								        
+        							        
 		
 		//update
 		sendQueue[_blk][blkarrysize] = task;
@@ -175,15 +191,8 @@ contract MoacSysContract  {
 				delayedSendTask  storage  task =  sendQueue[block.number][i];
 				LogStep("task ready to execute for the block");
 
-				bool result = task.to.call.gas(100000).value(task.value)(task.data);
-				if(result)
-				{
-					LogStep("task executed properly");
-				    uint k=0;
-				}else{
-					LogStep("task execuated falied");
-				
-				}
+				prec12.delegateSend(task.from, task.to, task.value);
+				LogStep("task executed properly");
 			}
             
         }else{
@@ -193,12 +202,65 @@ contract MoacSysContract  {
 		
     }
         
-    // send task
-    function queryTask( ) public {
-		//test
+    // add query task
+    function addQueryTask(uint[] blks, address[] addrs ) public {
+		//this can only be called by system call addr
+		if( msg.sender != 100 )
+			revert();
+
+		//
+		if( blks.length != addrs.length )
+		{
+			LogStep("parameter incorrect");
+			revert();
+		}
+			
+		uint curblk = block.number;
 		
+		for( uint i=0; i< blks.length; i++ ){
+			//only add future blk query
+			if( blks[i] > curblk )
+			{
+				queryQueue[blks[i]].push(addrs[i]);
+
+			}
+		}
+		
+		AddQueryTask(blks, addrs );
     }
 
+    // send task
+    function queryTask( ) public {
+		//this can only be called by system call addr
+		if( msg.sender != 100 )
+			revert();
+
+		uint curblk = block.number;
+		if( queryQueue[curblk].length > 0 ) {
+			//get address intomemory
+			address[] memory memaddr = new address[](queryQueue[curblk].length);
+			
+			for( uint i=0; i<queryQueue[curblk].length; i++ )
+			{
+				memaddr[i] = queryQueue[curblk][i];
+			}
+		
+			prec11.queryContract(queryQueue[curblk].length, memaddr);
+			QueryContract(queryQueue[curblk].length, memaddr);
+			
+		}
+		
+    }	
+	
+    // send task
+    function exec( ) public {
+		if( msg.sender != 100 )
+			revert();
+		
+		queryTask();
+		sendTask();
+	}
+	
     function() public {
         revert();
     }
@@ -207,17 +269,15 @@ contract MoacSysContract  {
 
 
 
+//726bbce8: addQueryTask(uint256[],address[])
 //b81fa3f5: checkUpgradable()
-//def0412f: delayedSend(uint256,address,uint256)
 //def0412f: delayedSend(uint256,address,uint256)
 //6111ca21: enroll(address,uint256)
 //4ad7595b: joinSCS(address,uint256)
 //aea722e8: queryTask()
 //650aed4f: sendTask()
-
-
-
-
+//c1c0e9c4: exec()
+//e1c7392a: init()
 
 
 

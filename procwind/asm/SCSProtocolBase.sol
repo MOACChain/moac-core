8cb58a0f8cec371dafd4caba932f4db733eafb86
mdranger <zhengpeng.li@moac.io>
2020-02-20 18:47:47 -0500

    Fixed the bug in the withdrawDone

--- procwind/asm/SCSProtocolBase.sol
+++ procwind/asm/SCSProtocolBase.sol
@@ -116,10 +116,10 @@ contract SCSProtocolBase {
 
     function withdraw() public {
         if (
-            scsList[msg.sender].state == uint(SCSStatus.withdrawPending)
+            scsList[msg.sender].state = uint(SCSStatus.withdrawPending)
             && block.number > (scsList[msg.sender].withdrawBlock + WITHDRAW_BLOCK_DELAY)
         ) {
-            scsList[msg.sender].state == uint(SCSStatus.withdrawDone);
+            scsList[msg.sender].state = uint(SCSStatus.withdrawDone);
             scsList[msg.sender].from.transfer(scsList[msg.sender].bond);
         }
     }
